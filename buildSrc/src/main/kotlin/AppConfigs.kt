7f6c903c64c13da58b33fc6d9de9aa3e275df0da
jiewang41 <jiewang41@iflytek.com>
2021-07-14 15:28:13 +0800

    项目配置(app): 0.1.5 版本发布
    重大改变: 无
    
    本次解决的问题: 无

--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -3,7 +3,6 @@
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import java.util.TimeZone
 
 /**
  * 应用配置
@@ -28,7 +27,7 @@ object AppConfigs {
     val versionCode = getVersionCode()
 
     /** 应用版本名 */
-    val versionName = "v0.1.4_$versionCode"
+    val versionName = "v0.1.5_$versionCode"
 }
 
 /** 根据日期时间获取对应版本号 */
