c73931df11b8967f4682508bc63a4efbc98becaf
WangJie <15555650921@163.com>
2023-10-29 21:18:53 +0800

    fix: 修复资产编辑，金额只能输入到小数点后一位问题

--- core/common/src/main/kotlin/cn/wj/android/cashbook/core/common/Patterns.kt
+++ core/common/src/main/kotlin/cn/wj/android/cashbook/core/common/Patterns.kt
@@ -1,7 +1,7 @@
 package cn.wj.android.cashbook.core.common
 
 /** 正则 - 金额，正负小数、整数 */
-const val PATTERN_SIGN_MONEY = "(-?)|(-?\\d+[.]?\\d?|(-?\\d+))"
+const val PATTERN_SIGN_MONEY = "(-?)|(-?\\d+[.]?\\d{0,2}|(-?\\d+))"
 
 /** 密码校验正则 必须包含大小写字母及数字，大于8位 */
 const val PASSWORD_REGEX =
