04247fcbcb490fcc7a7f61e0596afcca825bc391
WangJie <15555650921@163.com>
2023-11-09 00:06:51 +0800

    build: 构建逻辑优化，新增从环境变量获取版本逻辑

--- build-logic/convention/src/main/kotlin/cn/wj/android/cashbook/buildlogic/ApplicationSetting.kt
+++ build-logic/convention/src/main/kotlin/cn/wj/android/cashbook/buildlogic/ApplicationSetting.kt
@@ -1,7 +1,6 @@
 package cn.wj.android.cashbook.buildlogic
 
 import org.gradle.api.JavaVersion
-import org.gradle.api.Project
 
 /**
  * 应用配置数据
@@ -20,21 +19,42 @@ object ApplicationSetting {
         /** SDK 目标版本 */
         const val TARGET_SDK = 30
 
+        /** 大版本名 */
+        private const val VERSION_NAME = "v1.0.1"
+
         /** 版本号，动态生成 */
-        val versionCode = generateVersionCode()
+        val versionCode = getVersionCodeFromVersionName()
 
         /** 版本名，大版本号+版本号 */
-        val versionName = "v1.0.1_$versionCode"
+        val versionName = generateVersionName()
 
         /** 源码 jdk 版本 */
         val javaVersion = JavaVersion.VERSION_11
+
+        /** 从环境变量中获取版本名，若没有则根据当前时间生成 */
+        private fun generateVersionName(): String {
+            val buildTagName = System.getenv("BUILD_TAG_NAME") ?: ""
+            val versionName = buildTagName.ifBlank {
+                "${VERSION_NAME}_${generateVersionCode()}"
+            }.replace("_pre", "")
+            println("> Task :build-logic:generateVersionName buildTagName = <$buildTagName>, versionName = <$versionName>")
+            return versionName
+        }
 
         /** 根据日期时间获取对应版本号 */
         private fun generateVersionCode(): Int {
             val sdf = java.text.SimpleDateFormat("yyMMddHH", java.util.Locale.CHINA)
             val formatDate = sdf.format(java.util.Date())
             val versionCode = formatDate.toIntOrNull() ?: 10001
-            println("> Task :build-logic:getVersionCode formatDate: $formatDate versionCode: $versionCode")
+            println("> Task :build-logic:generateVersionCode formatDate: $formatDate versionCode: $versionCode")
+            return versionCode
+        }
+
+        private fun getVersionCodeFromVersionName(): Int {
+            val versionCode = runCatching {
+                versionName.split("_")[1].toInt()
+            }.getOrElse { generateVersionCode() }
+            println("> Task :build-logic:getVersionCodeFromVersionName versionCode: $versionCode")
             return versionCode
         }
     }
